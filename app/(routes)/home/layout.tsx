9e7896284b79032eb8227e7489112025f0edf8b0
ompateldeveloper <ompatel.developer@gmail.com>
2023-12-13 14:24:17 +0530

    redesigned landing,refactored folder structure

--- app/(routes)/home/layout.tsx
+++ app/(routes)/home/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
-import '../globals.css'
+import '../../globals.css'
 
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
