9ff88a712e0ab76a83ac54ec029a21460749b7b7
ompateldeveloper <ompatel.developer@gmail.com>
2023-12-15 18:01:55 +0530

    buton size updated

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,11 +18,13 @@ const buttonVariants = cva(
           "bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
         ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+        theme:'bg-gradient-to-r from-[#B235FF] to-[#FF3152] text-white font-bold hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80'
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        pill: "h-8 rounded-full px-4",
         icon: "h-10 w-10",
       },
     },
