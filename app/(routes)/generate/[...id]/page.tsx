4842eae3c225d8201c7fbc3424e4a3af76df81c5
utkarsh-1602 <usking619@gmail.com>
2023-12-23 15:42:19 +0530

    dynamic userId Route setup, error: userId getting undefined

--- app/(routes)/generate/[...id]/page.tsx
+++ app/(routes)/generate/[...id]/page.tsx
@@ -1,9 +1,6 @@
 'use client';
-import React, { useEffect } from 'react'
 export default function page({ params }: { params: { id: string } }) {
-    useEffect(()=>{
-    
-    },[])
+
     return (
     <div>
       {params.id}
