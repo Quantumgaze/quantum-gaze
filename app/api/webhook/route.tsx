a229b1d1f060d0e98959ee2b2a26d47651b83940
ompateldeveloper <ompatel.developer@gmail.com>
2023-12-25 20:18:14 +0530

    changed schema to add clarkuserid and removed logs model

--- app/api/webhook/route.tsx
+++ app/api/webhook/route.tsx
@@ -1,141 +1,114 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '@/lib/prisma'
- 
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import type { WebhookEvent } from "@clerk/clerk-sdk-node";
+import { prisma } from "@/lib/prisma";
+
 export async function POST(req: Request) {
- 
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
- 
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
- 
+
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
- 
+
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400
-    })
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
- 
+
   // Get the body
-  const payload = await req.json()
+  const payload = await req.json();
   const body = JSON.stringify(payload);
-  
- 
+
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
- 
-  let evt: WebhookEvent
- 
+
+  let evt: WebhookEvent;
+
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent
+    }) as WebhookEvent; 
   } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400
-    })
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
-  
-  // Get the ID and type
 
-function getUserDataFromEvent(evt:WebhookEvent) {
-  return {
-    clerkUserId: evt.data.id,
-    object:evt.data.object
-  };
-}
-
-async function handleUserCreated(evt:WebhookEvent) {
-
-  const newUser = getUserDataFromEvent(evt);
-  console.log(newUser);
-  prisma.logs.create({
-    data:{
-        body:"user created"
-    }
-  })
-  
-
-  try {
-    
-  } catch (err) {
-    console.error(`Failed to insert user: ${err}`);
+  async function handleSession(evt:WebhookEvent){
+    if(evt?.data) console.log(evt.data.id + " is active");
   }
-}
 
-async function handleUserUpdated(evt:WebhookEvent) {
-
-  const updatedUser = getUserDataFromEvent(evt);
-  console.log(updatedUser);
-  
-  prisma.logs.create({
-    data:{
-        body:"user updated"
-    }
-  })
-  try {
-
-    console.log("Successfully updated user!");
-  } catch (err) {
-    console.error(`Failed to update user: ${err}`);
-  }
-}
-
-async function handleUserDeleted(evt:WebhookEvent) {
-
-    const deletedUser = getUserDataFromEvent(evt);    
-    const Logs = await prisma.logs.create({
-        data: {
-          body:"user deleted"
-        }
-      })
+  async function handleUserCreated(evt:any) {
     try {
-  
-      console.log("Successfully updated user!");
+        await prisma.user.create({
+            data:{
+                clarkUserId:evt.data.id,
+                email: evt.data.email_addresses[0].email_address,
+                username: evt.data.username
+            }
+        });
+
+    } catch (err) {
+      console.error(`Failed to insert user: ${err}`);
+    }
+  }
+
+  async function handleUserUpdated(evt: any) {
+    try {
+        await prisma.user.update({
+            where: { clarkUserId: evt.data.id },
+            data: {
+                username: evt.data.username
+            }
+        });
     } catch (err) {
       console.error(`Failed to update user: ${err}`);
     }
   }
-exports = async function syncClerkData(request:NextRequest, response:NextResponse) {
 
-  switch (evt.type) {
-    case "user.created":
-      await handleUserCreated(evt);
-    //   response.status();
-      break;
-    case "user.updated":
-      await handleUserUpdated(evt);
-    //   response.setStatusCode(200);
-      break;
-    case "user.deleted":
-      await handleUserDeleted(evt);
-    //   response.setStatusCode(200);
-      break;
-    default:
-        prisma.logs.create({
-            data:{
-                body:"something bad happen"
-            }
-        })
-      console.log(`Unhandled event type: ${evt.type}`);
-    //   response.setStatusCode(400);
+  async function handleUserDeleted(evt: WebhookEvent) {
+    try {
+        await prisma.user.delete({
+            where: { clarkUserId: evt.data.id }
+        }); 
+    } catch (err) {
+      console.error(`Failed to update user: ${err}`);
+    }
   }
-
+  
+  async function syncClerkData() {
+    switch (evt.type) {
+      case "session.created":
+        await handleSession(evt);
+        break;
+      case "user.created":
+        await handleUserCreated(evt);
+        break;
+      case "user.updated":
+        await handleUserUpdated(evt);
+        break;
+      case "user.deleted":
+        await handleUserDeleted(evt);
+        break;
+      default:
+        console.log(`Unhandled event type: ${evt.type}`);
+    }
+  }
+  syncClerkData();
+  return new Response("", { status: 200 });
 }
-    
-}
- 
